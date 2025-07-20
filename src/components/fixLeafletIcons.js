bf600b9d014de3b26f195fabe663ca0af5c664a4
raj45alok <156644544+raj45alok@users.noreply.github.com>
2025-07-20 19:29:27 +0530

    Update fixLeafletIcons.js

--- src/components/fixLeafletIcons.js
+++ src/components/fixLeafletIcons.js
@@ -1,11 +1,10 @@
 
-// src/components/fixLeafletIcons.js
+
 import L from 'leaflet';
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
 import iconUrl from 'leaflet/dist/images/marker-icon.png';
 import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
 
-// Fix for default marker icons
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
